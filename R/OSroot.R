4219a048344c9df653e046dffb55941fd7f11658
Dan <dov.bcn@gmail.com>
2019-08-28 12:33:31 +0200

    changes in OSroot(), improvement on hardware detection

--- R/OSroot.R
+++ R/OSroot.R
@@ -6,8 +6,8 @@
 
 
 OSroot = function(OS=get_os()){
-  if(OS=='osx' & grepl('Air',Sys.info()['nodename'])) path = '/Users/user/Documents/Irsicaixa/' # Mac Dan
-  if(OS=='osx' & !grepl('Air',Sys.info()['nodename'])) path = '/Volumes/virievac/DAN/' # Mac Dan
+  if(OS=='osx' & grepl('Air',system('system_profiler SPHardwareDataType | grep "Model Identifier"',intern=T))) path = '/Users/user/Documents/Irsicaixa/' # Mac Dan
+  if(OS=='osx' & !grepl('Air',system('system_profiler SPHardwareDataType | grep "Model Identifier"',intern=T))) path = '/Volumes/virievac/DAN/' # Mac Dan
   # if(OS=='windows') path = 'Z:/' # PC?
   # if(OS=='linux') path = '/run/user/868803867/gvfs/smb-share:server=irsi-clufs,share=virievac/DAN/' # Server
   return(path)
