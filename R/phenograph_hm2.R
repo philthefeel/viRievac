5d7434d13eb4d312d8245fb4364f38a9bcc38102
Dan Ouchi <douchi@irsi-sc-m110.local>
2020-04-15 11:28:26 +0200

    new phenograph function

--- R/phenograph_hm2.R
+++ R/phenograph_hm2.R
@@ -1,24 +1,22 @@
 
 
-# Create a Heatnap from Rphenograph results:
-# data must have a variable named 'louvain', the output from Rphenograph()
 
+# phenograph_hm2 ----------------------------------------------------------
 
-phenograph_hm = function(data,
-                         group.var,
-                         vars,
-                         color=colorRampPalette(c("blue4","#0571B0","white","red1","#CA0020"))(100),
-                         statistic='median'){
+phenograph_hm2 = function(data,
+                          group.var,
+                          vars,
+                          color=colorRampPalette(c("blue4","#0571B0","white","red1","#CA0020"))(100),
+                          statistic='median'){
 
-  aux = data %>% dplyr::select(group.var,vars) %>%
-    group_by_(group.var)
+  aux = data %>% dplyr::select(all_of(group.var),all_of(vars)) %>% na.omit() %>% group_by_(group.var)
   if(statistic=='median') aux = aux %>% dplyr::summarise_all(median) %>% select(-group.var) %>% t() %>% data.frame
   if(statistic=='mean') aux = aux %>% dplyr::summarise_all(mean) %>% select(-group.var) %>% t() %>% data.frame
   colnames(aux) = table(data[,group.var]) %>% names
 
   # Calculate cluster frequencies
-  clustering_table <- as.numeric(table(data %>% select(group.var)))
-  labels_col <- paste0(colnames(aux), " (",
+  clustering_table <- as.numeric(table(data %>% select(all_of(group.var))))
+  labels_col <- paste0(colnames(aux), " (",formatC(clustering_table,format="g"),', ',
                        round(clustering_table / sum(clustering_table) * 100, 2), "%)")
 
   # heatmap parameters
